ab880c19cc81cc194b6b9c72f54a28a426a3082a
babossene <senebabos1986@gmail.com>
2026-06-27 20:45:14 +0100

    first

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import { Routes, Route } from "react-router"
+import './App.css'
+import Home from './components/Home'
+import Create from './components/Create'
+import Edit from './components/Edit'
+import Delete from './components/Delete'
+function App() {
+  return (
+    <>
+      <Routes>  
+        <Route path="" element={<Home />} />
+        <Route path="create" element={<Create />} />
+        <Route path="edit/:id" element={<Edit />} />
+        <Route path="delete/:id" element={<Delete />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
